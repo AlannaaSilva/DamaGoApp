e462a8f06f166367c6930e0d6b94569834283950
eusamir <samirandradee@gmail.com>
2024-11-04 16:08:25 -0300

    fix: ajustando navigação

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,12 @@
 import "react-native-gesture-handler";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./(tabs)/index";
+import HomeUserScreen from "./(tabs)/HomeUserScreen";
+import Register from "./(stack)/Register";
+import EventDetailsScreen from "./(stack)/EventDetailsScreen";
+
 // Definindo o tipo para os parâmetros do stack
 export type RootStackParamList = {
   Home: undefined; // Nenhum parâmetro
@@ -10,14 +15,17 @@ export type RootStackParamList = {
   EventDetails: undefined; // Exemplo de parâmetro
 };
 
+const Stack = createStackNavigator<RootStackParamList>(); // Aplicando o tipo ao Stack
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Screen name="Home" />
-        <Stack.Screen name="HomeUser" />
-        <Stack.Screen name="Register" />
-        <Stack.Screen name="EventDetails"/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="HomeUser" component={HomeUserScreen} />
+        <Stack.Screen name="Cadastro" component={Register} />
+        <Stack.Screen name="EventDetails" component={EventDetailsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
